4b785b5ad8eab070be094250616361845bb81417
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-05-29 00:14:11 -0500

    added Person Class

---
+++ Composition/Person.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "Glasses.h"
+using namespace std;
+
+class Person
+{
+private:
+	// Attributes
+	string name;
+	string ssn;
+	Glasses myGlasses;
+
+public:
+	// Constructors and Destructor
+	Person();
+	Person(string name, string ssn, Glasses currentGlasses);
+	~Person();
+
+	// Behaviors
+	string toString(void);
+	string writeToFile(void);
+	string readToFile(void);
+
+	// Accessors and Mutators
+	string getName(void);
+	void setName(string name);
+
+	string getSSN(void);
+	void setSSN(string ssn);
+
+	Glasses getMyGlasses(void);
+	void setMyGlasses(Glasses currentGlasses);
+};
+
