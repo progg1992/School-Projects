514f300be7effac5880862589ed42a2f13593963
Pierce Rogg <pierce.rogg@mymail.nwtc.edu>
2021-05-28 23:26:36 -0500

    intial commit and added Glasses Class

---
+++ Composition/Source.cpp
@@ -0,0 +1,21 @@
+/*
+Pierce Rogg
+CIS 247C
+Week 4 - Composition
+5/28/2021
+*/
+
+// Required Libraries
+#include <iostream>
+#include <conio.h>
+#include <string>
+using namespace std;
+
+/// Entry point to the application
+int main()
+{
+	// Pause
+	cout << "\nPress any key to continue...";
+	_getch();
+	return 0;
+}
